0c6aec4f485f1a934daf970661efcd9725d1ce4e
asrytikov <asrytikov@gmail.com>
2023-11-08 12:10:26 +0400

    Lesson26

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,5 +1,35 @@
+package p1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://www.ya.ru");
+        InputStream inputStream = url.openStream();
+        Scanner scanner = new Scanner(inputStream, "UTF-8");
+        //[схема:]спец часть[фрагмент]
+
+        /*../../java/index.html
+        http://docs.google.com/api/java/index.html#Socket()
+        /java/index.html#Socket()*/
+
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setDoOutput(true);
+
+
+        String input = "alex:123456";
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encoding = encoder.encodeToString(input.getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoding);
+        urlConnection.setRequestProperty("Autorization", "Basic " + encoding);
+        urlConnection.connect();
     }
 }
--- src/p2/Main.java
+++ src/p2/Main.java
@@ -1,2 +1,46 @@
-package p2;public class Main {
+package p2;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            String urlName = "http://pravo.gov.ru/";
+            URL url = new URL(urlName);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+            Map<String, List<String>> headers = urlConnection.getHeaderFields();
+            for (Map.Entry<String, List<String>> entry : headers.entrySet()){
+                String key = entry.getKey();
+                for(String value: entry.getValue()){
+                    System.out.println(key + ": " + value);
+                }
+            }
+            System.out.println("-----------------");
+            System.out.println("getContentType: " + urlConnection.getContentType());
+            System.out.println("getContentLength: " + urlConnection.getContentLength());
+            System.out.println("getContentEncoding: " + urlConnection.getContentEncoding());
+            System.out.println("getDate:" + urlConnection.getDate());
+            System.out.println("------------------");
+
+            String enc = urlConnection.getContentEncoding();
+            if (enc==null) enc = "UTF-8";
+            try(Scanner scanner = new Scanner(urlConnection.getInputStream(), enc)){
+                for(int n=1; scanner.hasNextLine() && n<=10; n++){
+                    System.out.println(scanner.nextLine());
+                }
+                if (scanner.hasNextLine()) System.out.println(".........");
+            }
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
 }
